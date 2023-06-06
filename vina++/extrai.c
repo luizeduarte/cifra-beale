32bfbbe2983511c0fe8f5030925725f54bc59265
Luize Duarte <luize.lcd@hotmail.com>
2023-06-06 09:25:40 -0300

    att

--- vina++/extrai.c
+++ vina++/extrai.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_STRING 1024
 //lê muitas vezes o numero de arquivos e o tamanho do conteudo, 
 //verificar quando fizer o main se continua mesmo repetitivo, 
 //talvez criar uma função, mas ela faria mts coisas, talvez uma struct
@@ -18,15 +19,15 @@ struct diretorio{
 };
 
 struct diretorio** le_diretorio(FILE* archive){
-	int num_arquivos, tam_nome, tamanho;
+	int num_arq, tam_nome, tamanho;
 	struct diretorio* *v_diretorio;
 
-	fread(&num_arquivos, sizeof(int), 1, archive);		//le o numero de arquivos
-	v_diretorio = malloc(num_arquivos * sizeof(struct diretorio*));
+	fread(&num_arq, sizeof(int), 1, archive);		//le o numero de arquivos
+	v_diretorio = malloc(num_arq * sizeof(struct diretorio*));
 	fread(&tamanho, sizeof(long long int), 1, archive);		//le o tamanho dos conteudos
 	fseek(archive, (tamanho + sizeof(long long int) + sizeof(int)), SEEK_SET);	//vai para o diretorio
 
-	for (int i = 0; i < num_arquivos; i++){
+	for (int i = 0; i < num_arq; i++){
 		v_diretorio[i] = malloc(sizeof(struct diretorio));
 		fread(&tam_nome, sizeof(int), 1, archive);	//le o tamanho do nome do arquivo
 		v_diretorio[i]->nome = malloc(tam_nome * sizeof(char));
@@ -41,58 +42,68 @@ struct diretorio** le_diretorio(FILE* archive){
 	return v_diretorio;
 }
 
-int info_arquivo(FILE* archive, char* nome_arquivo, struct diretorio* v_diretorio[]){
-	//recebe o nome de um arquivo e retorna o indice dele no vetor diretorio
+int id_arquivo(FILE *archive, char *nome_arquivo, struct diretorio *v_diretorio[])
+{
+	//recebe o archive e o nome de um arquivo, retornando o indice dele no vetor diretorio
 
-	int achou = 0, num_arquivos;
+	int achou = 0, num_arq;
 
 	fseek(archive, 0, SEEK_SET);
-	fread(&num_arquivos, sizeof(int), 1, archive);		//le o numero de arquivos
-	while ((achou == 0) && (num_arquivos > 0)){
-		if (strcmp(v_diretorio[num_arquivos - 1]->nome, nome_arquivo) == 0){
+	fread(&num_arq, sizeof(int), 1, archive);		//le o numero de arquivos
+	while ((achou == 0) && (num_arq > 0)){
+		if (strcmp(v_diretorio[num_arq - 1]->nome, nome_arquivo) == 0){
 			achou = 1;
 		}
 
-		num_arquivos--;
+		num_arq--;
 	}
 
 	if (achou == 0)
 		return -1;
 
-	return num_arquivos;
+	return num_arq;
+}
+
+void copia_texto(FILE* archive, struct diretorio* v_diretorio[], int arquivo, FILE* arq_novo){
+	char* buffer = malloc(sizeof(char) * MAX_STRING);
+	long long int tam_texto = v_diretorio[arquivo]->tamanho;
+	long long int pos_texto = v_diretorio[arquivo]->posicao;
+	FILE* arq_le = archive;
+	FILE* arq_escreve = arq_novo;
+	
+	//calcula o numeros de blocos a serem lidos
+	long long int num_blocos = tam_texto / MAX_STRING;
+	int resto = tam_texto % MAX_STRING;
+
+	fseek(arq_le, pos_texto, SEEK_SET);	//vai ate o comeco do texto a ser copiado
+	for (int i = 0; i < num_blocos; i++){
+		fread(buffer, sizeof(char), MAX_STRING, arq_le);
+		fwrite(buffer, sizeof(char), MAX_STRING, arq_escreve);
+	}
+	fread(buffer, sizeof(char), resto, arq_le);
+	fwrite(buffer, sizeof(char), resto, arq_escreve);
 }
 
 void extrai(FILE* archive, char* nome_arquivo, struct diretorio* v_diretorio[]){	//verificar permissoes
 	int arquivo;
-	char* buffer = malloc(sizeof(char) * 1024);
 
-	FILE* novo_arquivo = fopen(nome_arquivo, "w");
+	FILE* arq_novo = fopen(nome_arquivo, "w");
+	v_diretorio = le_diretorio(archive);
 
-	arquivo = info_arquivo(archive, nome_arquivo, v_diretorio);
+	arquivo = id_arquivo(archive, nome_arquivo, v_diretorio);
 	if (arquivo < 0){
-		fprintf(stderr, "Arquivo nao encontrado\n");
+		fprintf(stderr, "Arquivo nao encontrado no archive\n");
 		return;
 	}
 
-	//calcula o numeros de blocos a serem lidos
-	long long int num_blocos = v_diretorio[arquivo]->tamanho / 1024;
-	int resto = v_diretorio[arquivo]->tamanho % 1024;
 
-	fseek(archive, v_diretorio[arquivo]->posicao, SEEK_SET);
-	//copia do archive para o novo arquivo
-	for (int i = 0; i < num_blocos; i++){
-		fread(buffer, sizeof(char), 1024, archive);
-		fwrite(buffer, sizeof(char), 1024, novo_arquivo);
-	}
-	fread(buffer, sizeof(char), resto, archive);
-	fwrite(buffer, sizeof(char), resto, novo_arquivo);
 
-	fclose(novo_arquivo);
+	fclose(arq_novo);
 }
 
 int main(){
 	FILE* archive = fopen("archive.bin", "rb");
-	struct diretorio* *v_diretorio = le_diretorio(archive);
+	struct diretorio* *v_diretorio = NULL;
 	extrai(archive, "teste.txt", v_diretorio);
 	return 0;
 }
